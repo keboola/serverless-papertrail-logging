1b65b06a30f34ce14ad7abc785f856d1301a2a0d
Jakub Matejka <jakub@keboola.com>
2017-11-14 16:17:55 +0100

    :zap: Init

---
+++ src/index.js
@@ -0,0 +1,136 @@
+'use strict';
+
+const _ = require('lodash');
+const fs = require('fs');
+const path = require('path');
+
+class PapertrailLogging {
+  constructor(serverless) {
+    this.serverless = serverless;
+    this.service = serverless.service;
+
+    this.provider = this.serverless.getProvider('aws');
+
+    this.hooks = {
+      'before:deploy:createDeploymentArtifacts': this.beforeDeployCreateDeploymentArtifacts.bind(this),
+      'deploy:compileEvents': this.deployCompileEvents.bind(this),
+      'after:deploy:deploy': this.afterDeployDeploy.bind(this),
+    };
+  }
+
+  static getFunctionName() {
+    return 'papertrailLogger';
+  }
+
+  static getFunctionFilename() {
+    return 'papertrail-logger-function';
+  }
+
+  getEnvFilePath() {
+    return path.join(this.serverless.config.servicePath, LogSubscription.getFunctionFilename());
+  }
+
+  beforeDeployCreateDeploymentArtifacts() {
+    this.serverless.cli.log('Creating temporary logger function...');
+    let functionPath = this.getEnvFilePath();
+
+    if (!fs.existsSync(functionPath)) {
+      fs.mkdirSync(functionPath);
+    }
+
+    _.merge(
+      this.service.provider.compiledCloudFormationTemplate.Resources,
+      {
+        PapertrailLoggerLogGroup: {
+          Type: "AWS::Logs::LogGroup",
+          Properties: {
+            LogGroupName: `/aws/lambda/${this.service.service}-${this.service.provider.stage}-${LogSubscription.getFunctionName()}`
+          }
+        }
+      }
+    );
+
+    let templatePath = path.resolve(__dirname, './logger.handler.js');
+    let templateFile = fs.readFileSync(templatePath, 'utf-8');
+
+    let handlerFunction = templateFile
+      .replace('%papertrailPort%', this.service.custom.papertrail.port)
+      .replace('%papertrailHostname%', this.service.service)
+      .replace('%papertrailProgram%', this.service.provider.stage);
+    fs.writeFileSync(path.join(functionPath, 'handler.js'), handlerFunction);
+    this.service.functions[LogSubscription.getFunctionName()] = {
+      handler: `${LogSubscription.getFunctionFilename()}/handler.handler`,
+      name: `${this.service.service}-${this.service.provider.stage}-${LogSubscription.getFunctionName()}`,
+      tags: _.has(this.service.provider, 'stackTags') ? this.service.provider.stackTags : {},
+      events: []
+    };
+  }
+
+  deployCompileEvents() {
+    this.serverless.cli.log('Creating log subscriptions...');
+
+    const loggerLogicalId = this.provider.naming.getLambdaLogicalId(LogSubscription.getFunctionName());
+
+    _.each(this.service.provider.compiledCloudFormationTemplate.Resources, (item, key) => {
+      if (_.has(item, 'Type') && item.Type === 'AWS::Logs::LogGroup') {
+        this.service.provider.compiledCloudFormationTemplate.Resources[key].Properties.RetentionInDays = 30;
+      }
+    });
+
+    _.merge(
+      this.service.provider.compiledCloudFormationTemplate.Resources,
+      {
+        LambdaPermissionForSubscription: {
+          Type: 'AWS::Lambda::Permission',
+          Properties: {
+            FunctionName: { 'Fn::GetAtt': [loggerLogicalId, 'Arn'] },
+            Action: 'lambda:InvokeFunction',
+            Principal: { 'Fn::Sub': 'logs.${AWS::Region}.amazonaws.com' },
+            SourceArn: { 'Fn::Sub': 'arn:aws:logs:${AWS::Region}:${AWS::AccountId}:log-group:/aws/lambda/*' },
+          },
+          DependsOn: [loggerLogicalId]
+        }
+      }
+    );
+
+    const functions = this.service.getAllFunctions();
+    functions.forEach((functionName) => {
+      if (functionName !== LogSubscription.getFunctionName()) {
+        const functionData = this.service.getFunction(functionName);
+        const normalizedFunctionName = this.provider.naming.getNormalizedFunctionName(functionName);
+        _.merge(
+          this.service.provider.compiledCloudFormationTemplate.Resources,
+          {
+            [`${normalizedFunctionName}SubscriptionFilter`]: {
+              Type: 'AWS::Logs::SubscriptionFilter',
+              Properties: {
+                DestinationArn: { 'Fn::GetAtt': [loggerLogicalId, "Arn"] },
+                FilterPattern: '',
+                LogGroupName: `/aws/lambda/${functionData.name}`,
+              },
+              DependsOn: ['LambdaPermissionForSubscription']
+            }
+          }
+        );
+      }
+    });
+  }
+
+  afterDeployDeploy() {
+    this.serverless.cli.log('Removing temporary logger function');
+    let functionPath = this.getEnvFilePath();
+
+    try {
+      if (fs.existsSync(functionPath)) {
+        if (fs.existsSync(path.join(functionPath, 'handler.js'))) {
+          fs.unlinkSync(path.join(functionPath, 'handler.js'));
+        }
+        fs.rmdirSync(functionPath);
+      }
+    } catch (err) {
+      throw new Error(err);
+    }
+  }
+}
+
+module.exports = PapertrailLogging;
---
+++ src/logger.handler.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const _ = require('lodash');
+const papertrail = require('winston-papertrail').Papertrail;
+const winston = require('winston');
+const zlib = require('zlib');
+
+exports.handler = function (event, context, callback) {
+  const logger = new (winston.Logger)({
+    transports: [],
+  });
+  logger.add(papertrail, {
+    host: 'logs.papertrailapp.com',
+    port: '%papertrailPort%',
+    hostname: '%papertrailHostname%',
+    program: '%papertrailProgram%',
+    flushOnClose: true,
+    includeMetaInMessage: false,
+    handleExceptions: true,
+    humanReadableUnhandledException: false,
+    logFormat: (level, message) => {
+      const consoleLog = message.split('\t');
+      if (consoleLog.length === 3) {
+        try {
+          const logData = JSON.parse(consoleLog[2]);
+          if (_.has(logData.event, 'body') && _.isString(logData.event.body)) {
+            logData.event.body = JSON.parse(logData.event.body);
+          }
+          return JSON.stringify(logData);
+        } catch (e) {
+          return JSON.stringify({ requestId: consoleLog[1], log: consoleLog[2] });
+        }
+      }
+
+      try {
+        const logData = JSON.parse(message);
+        if (!_.has(logData, 'statusCode')) {
+          logData.statusCode = 500;
+        }
+        return JSON.stringify(logData);
+      } catch (e) {
+        return message;
+      }
+    },
+  });
+
+  const payload = new Buffer(event.awslogs.data, 'base64');
+  zlib.gunzip(payload, (err, result) => {
+    if (err) {
+      return callback(err);
+    }
+
+    const logData = JSON.parse(result.toString('utf8'));
+    if (logData.messageType === 'CONTROL_MESSAGE') {
+      return callback();
+    }
+
+    logData.logEvents.forEach((line) => {
+      if (line.message && !_.startsWith(line.message, 'START RequestId') && !_.startsWith(line.message, 'END RequestId')
+        && !_.startsWith(line.message, 'REPORT RequestId')) {
+        logger.info(line.message);
+      }
+    });
+
+    logger.close();
+    return callback();
+  });
+};
